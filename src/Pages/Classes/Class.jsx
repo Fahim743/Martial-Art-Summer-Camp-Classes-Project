4e5f91a24df16d0025477e8f8dd5ea581ea4d9d5
Fahim743 <fahimahmadul@gmail.com>
2023-06-17 01:34:16 +0600

    Remove function add in MyselectedClass for Student

--- src/Pages/Classes/Class.jsx
+++ src/Pages/Classes/Class.jsx
@@ -1,13 +1,83 @@
 import { useEffect, useState } from "react";
+import checkAdmin from "../../Hooks/checkAdmin";
+import checkInstructor from "../../Hooks/checkInstructor";
+import { useContext } from "react";
+import { AuthContext } from "../../Provider/AuthProvider";
+import Swal from "sweetalert2";
+import useAxios from "../../Hooks/useAXios";
 
 const Class = () => {
+  const {user} = useContext(AuthContext)
   const [classes, setClasses] = useState([]);
+
+  const [isAdmin] = checkAdmin();
+  const [ isInstructor] = checkInstructor();
+  const [axiosSecure] = useAxios();
 
   useEffect(() => {
     fetch(`https://martial-art-summer-camp-server-side.vercel.app/classes`)
       .then((res) => res.json())
       .then((data) => setClasses(data));
   }, []);
+
+  const handleSendClass = (id) => {
+    if (!user) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Please login before selecting a class!",
+      });
+      // navigate(from, { replace: true });
+    } else if (user) {
+      console.log(id);
+      const filter = classes.find((item) => item._id === id);
+      const {
+        classPhoto,
+        className,
+        instructorEmail,
+        instructorName,
+        price,
+        availableSeats,
+        status,
+        _id,
+      } = filter;
+      const choosenItems = {
+        _id,
+        classPhoto,
+        className,
+        instructorEmail,
+        instructorName,
+        price,
+        availableSeats,
+        status,
+        studentCart: [user?.email],
+      };
+      console.log(choosenItems);
+
+      axiosSecure
+        .post("/classCart", choosenItems)
+        .then((response) => {
+          if (response.data.insertedId) {
+            Swal.fire({
+              position: "center",
+              icon: "success",
+              title: "Added to cart",
+              showConfirmButton: false,
+              timer: 1700,
+            });
+          }
+        })
+        .catch((error) => {
+          console.log("Error response:", error.response); 
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Please select another one!",
+            footer: "Please select another class",
+          });
+        });
+    }
+  };
 
   return (
     <>
@@ -24,9 +94,12 @@ const Class = () => {
                 <p>Available seats: {allClass.availableSeats}</p>
                 <p>Price: {allClass.price}</p>
                 <div className="card-actions justify-end">
-                 { <button className="btn btn-primary" disabled="true">
+                  <button
+                    onClick={() => handleSendClass(allClass._id)}
+                  className="btn btn-primary" 
+                  disabled={isAdmin || isInstructor || allClass.availableSeats === 0 ? true : false  }>
                     Select
-                  </button>}
+                  </button>
                 </div>
               </div>
             </div>
--- src/Pages/Dashboard/Student/MySelectedClass.jsx
+++ src/Pages/Dashboard/Student/MySelectedClass.jsx
@@ -1,4 +1,49 @@
+import { useContext } from "react";
+import useAxios from "../../../Hooks/useAXios";
+import { AuthContext } from "../../../Provider/AuthProvider";
+import { useQuery } from "@tanstack/react-query";
+import Swal from "sweetalert2";
+
 const MySelectedClass = () => {
+  const [axiosSecure] = useAxios();
+  const { user } = useContext(AuthContext);
+
+  const { data: classCart = [], refetch } = useQuery(
+    ["classCart"],
+    async () => {
+      const res = await axiosSecure.get(`/classCart/${user?.email}`);
+      return res.data;
+    }
+  );
+  const handleRemove = (id) => {
+    if (!user) {
+      alert("Please Sign In");
+    } else {
+      axiosSecure
+        .delete(`/classCart/${id}/${user?.email}`)
+        .then((response) => {
+          console.log("Course deleted:", response.data);
+          refetch();
+          Swal.fire({
+            position: "center",
+            icon: "success",
+            title: "Class Deleted  Successfully",
+            showConfirmButton: false,
+          
+          });
+        })
+        .catch((error) => {
+          console.log("Error when deleting class:", error);
+          // Show an error message
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Error while removing the class!",
+           
+          });
+        });
+    }
+  };
   return (
     <div>
       <div className="overflow-x-auto">
@@ -13,34 +58,40 @@ const MySelectedClass = () => {
             </tr>
           </thead>
           <tbody>
-            {/* row 1 */}
-            <tr>
-              <td>
-                <div className="flex items-center space-x-3">
-                  <div className="avatar">
-                    <div className="mask mask-squircle w-12 h-12">
-                      <img
-                        src="/tailwind-css-component-profile-2@56w.png"
-                        alt="Avatar Tailwind CSS Component"
-                      />
+            {classCart.map((single) => (
+              <tr key={single._id}>
+                <td>
+                  <div className="flex items-center space-x-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img
+                          src={single.classPhoto}
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
                     </div>
                   </div>
-                </div>
-              </td>
-              <td>
-                <div>
-                  <div className="font-bold">Hart Hagerty</div>
-                 
-                </div>
-              </td>
-              <td>
-               <button className="btn bg-red-700 text-white btn-xs">Remove </button>
-              </td>
-              <td className="text-center">
-                <button className="btn btn-success text-white btn-xs ">Pay</button>
-              </td>
-              
-            </tr>
+                </td>
+                <td>
+                  <div>
+                    <div className="font-bold">{single.className}</div>
+                  </div>
+                </td>
+                <td>
+                  <button
+                    onClick={() => handleRemove(single._id)}
+                    className="btn bg-red-700 text-white btn-xs"
+                  >
+                    Remove{" "}
+                  </button>
+                </td>
+                <td className="text-center">
+                  <button className="btn btn-success text-white btn-xs ">
+                    Pay
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
